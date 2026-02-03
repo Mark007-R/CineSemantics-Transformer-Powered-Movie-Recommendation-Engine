1e68184ad800973a1aa182b78d641705d88df7c8
Mark007-R <markrodrigues2004@gmail.com>
2026-02-03 18:36:42 +0530

    updated to necessary imports

--- utils/milvus_vectordb.py
+++ utils/milvus_vectordb.py
@@ -1,9 +1,4 @@
 import logging
-import torch
-import numpy as np
-import pandas as pd
-from sentence_transformers import SentenceTransformer
-from tqdm import tqdm
 from text_embedder import embed_text
 from pymilvus import connections, utility, FieldSchema, CollectionSchema, DataType, Collection
 
