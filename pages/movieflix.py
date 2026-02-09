705e2317c7b138191e26bd42f23adb1ddc1c1473
Mark007-R <markrodrigues2004@gmail.com>
2026-02-09 23:45:58 +0530

    style reversed

--- pages/movieflix.py
+++ pages/movieflix.py
@@ -278,7 +278,10 @@ st.markdown("""
         border: 1px solid rgba(255, 255, 255, 0.1);
     }
     
-    
+    ::-webkit-scrollbar {
+        width: 10px;
+        height: 10px;
+    }
     
     ::-webkit-scrollbar-track {
         background: rgba(255, 255, 255, 0.05);
