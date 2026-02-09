774b3c7de6f6fa0c039c8f742e36e9721f80d9f2
Mark007-R <markrodrigues2004@gmail.com>
2026-02-09 23:45:27 +0530

    style change

--- pages/movieflix.py
+++ pages/movieflix.py
@@ -278,10 +278,7 @@ st.markdown("""
         border: 1px solid rgba(255, 255, 255, 0.1);
     }
     
-    ::-webkit-scrollbar {
-        width: 10px;
-        height: 10px;
-    }
+    
     
     ::-webkit-scrollbar-track {
         background: rgba(255, 255, 255, 0.05);
